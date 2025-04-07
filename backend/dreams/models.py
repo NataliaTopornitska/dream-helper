f70c41843f5bc04fe3cad3605f19495d76c04f25
Marharyta Zazulia <zazmargo@gmail.com>
2025-04-07 14:49:54 -0300

    add models: City, Country, User (Django), UserProfile, DreamerProfile & ActivationToken

--- backend/dreams/models.py
+++ backend/dreams/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        verbose_name_plural = "countries"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="cities"
+    )
+
+    class Meta:
+        verbose_name_plural = "cities"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,96 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
+from django.db import models
+from django.utils.translation import gettext as _
+
+from backend.dreams.models import City
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    is_owner = models.BooleanField(_("is owner"), default=False)
+    is_donator = models.BooleanField(_("is donator"), default=False)
+    is_active = models.BooleanField(_("is active"), default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+class ActivationToken(models.Model):
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=30, blank=True, null=True, unique=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+    direction = models.CharField(max_length=150, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    avatar = models.URLField(null=True, blank=True)     # save in Storage
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class DreamerProfile(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True, null=True, unique=True)
+    phone_number = models.CharField(max_length=30, unique=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+    direction = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
