4d1a3624ead2a33b740512136d09bf1e73b68635
Natalia <nataliia.topornitska@gmail.com>
2025-04-09 14:57:05 +0300

    initial-commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+export const App: React.FC = () => {
+  return (
+    <div className="starter">
+      <h1>Dreamhelper</h1>
+    </div>
+  );
+};
