2283b0aed84abcc750bd70b1cba5f7b3572f0672
Marharyta Zazulia <zazmargo@gmail.com>
2025-04-10 13:06:40 -0300

    add endpoints for dreams/

--- backend/dreams/urls.py
+++ backend/dreams/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import CategoryView
-
+from .views import CategoryView, DreamViewSet
 
 app_name = "dreams"
 
 router = DefaultRouter()
 router.register(r"categories", CategoryView, basename="category")
+router.register(r"dreams", DreamViewSet)
 
 
 urlpatterns = [
--- backend/dreams/views.py
+++ backend/dreams/views.py
@@ -1,9 +1,20 @@
+import os
+
 from rest_framework import mixins
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from dreams.models import Category
-from dreams.serializers import CategorySerializer
+from dreams.models import Category, Dream
+from dreams.serializers import (
+    CategorySerializer,
+    DreamCreateSerializer,
+    DreamBaseSerializer,
+)
+
+from utils.email import send_email_with_template
+
+from app import settings
 
 
 class CategoryView(
@@ -17,3 +28,65 @@ class CategoryView(
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAdminUser,)
+
+
+class DreamViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet,
+):
+    queryset = Dream.objects.all()
+    serializer_class = DreamBaseSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        # every time, if watch detail of Dream, number_views +1
+        instance = self.get_object()
+        instance.number_views += 1
+        instance.save(update_fields=["number_views"])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        dream = self.serializer_class.Meta.model.objects.get(pk=response.data["id"])
+
+        context = {
+            "owner": request.user,
+            "application": dream.title,
+        }
+
+        send_email_with_template(
+            subject="Your request for Dream fulfillment has been received",
+            template_name=os.path.join(
+                settings.BASE_DIR,
+                "templates",
+                "email",
+                "confirm_received_new_dream_email.html",
+            ),
+            context=context,
+            recipient_email=dream.owner.email,
+        )
+        # mark for owner: is_owner = True
+        owner = dream.owner
+        owner.is_owner = True
+        owner.save()
+
+        return response
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return DreamBaseSerializer
+        if self.action == "retrieve":
+            return DreamBaseSerializer
+        if self.action == "create":
+            return DreamCreateSerializer
+        # if self.action == "upload_dream_photo":
+        #     return DreamPhotoSerializer
+        # if self.action == "update":
+        #     return DreamUpdateSerializer
+        # if self.action == "make_donation":
+        #     return AddDonationSerializer
+        return DreamBaseSerializer
