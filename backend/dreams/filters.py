a5ebc26e9c078645b69398774969bbde29066981
Marharyta Zazulia <zazmargo@gmail.com>
2025-04-21 15:47:33 -0300

    change goal_range in DreamFilter to choose from multiple options (user does not need to enter numbers)

--- backend/dreams/filters.py
+++ backend/dreams/filters.py
@@ -1,12 +1,28 @@
 import django_filters
 from django.db.models import Q, F
 
-from .models import Dream
+from .models import Dream, Category
 from users.models import City, Country
 
 
 class DreamFilter(django_filters.FilterSet):
-    goal_range = django_filters.RangeFilter(field_name="goal", label="Goal Range")
+    GOAL_CHOICES = (
+        ("until_100", "Until 100"),
+        ("101_500", "101 - 500"),
+        ("501_1000", "501 - 1000"),
+        ("more_1000", "More than 1000"),
+    )
+
+    category = django_filters.ModelChoiceFilter(
+        queryset=Category.objects.all(),
+        field_name="categories",
+        label="Category",
+    )
+    goal_range = django_filters.ChoiceFilter(
+        choices=GOAL_CHOICES,
+        method="filter_goal_range",
+        label="Goal Range",
+    )
     country = django_filters.ModelChoiceFilter(
         queryset=Country.objects.all(),
         field_name="country",
@@ -23,7 +39,7 @@ class DreamFilter(django_filters.FilterSet):
         model = Dream
         fields = (
             "status",
-            "categories",
+            "category",
             "goal_range",
             "country",
             "city",
@@ -66,3 +82,14 @@ class DreamFilter(django_filters.FilterSet):
         return queryset.filter(
             Q(dreamer__city=value) | Q(owner__userprofile__city=value)
         )
+
+    def filter_goal_range(self, queryset, name, value):
+        if value == "until_100":
+            return queryset.filter(goal__lte=100)
+        elif value == "101_500":
+            return queryset.filter(goal__gte=101, goal__lte=500)
+        elif value == "501_1000":
+            return queryset.filter(goal__gte=501, goal__lte=1000)
+        elif value == "more_1000":
+            return queryset.filter(goal__gt=1000)
+        return queryset
