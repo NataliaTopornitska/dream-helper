3b000a2aebba04c4ecb37a50ac95a49221e7effd
Marharyta Zazulia <zazmargo@gmail.com>
2025-06-26 12:56:24 -0300

    change set_cors for frontend

--- backend/set-cors.sh
+++ backend/set-cors.sh
@@ -10,7 +10,7 @@ mc anonymous set download local/dreams-media
 mc cors set local/dreams-media <<EOF
 [
   {
-    "AllowedOrigins": ["http://localhost:5173"],
+    "AllowedOrigins": ["http://localhost:5173", "http://127.0.0.1:5173"],
     "AllowedMethods": ["GET"],
     "AllowedHeaders": ["*"],
     "ExposeHeaders": [],
