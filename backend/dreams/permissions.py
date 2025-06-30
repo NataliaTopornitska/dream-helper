3fe35425212d3723b6f160dd1e6ce87d899fc993
Marharyta Zazulia <zazmargo@gmail.com>
2025-06-30 17:57:27 -0300

    fix permissions for DreamDetail for owner & Admin, when status Application

--- backend/dreams/permissions.py
+++ backend/dreams/permissions.py
@@ -2,8 +2,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAdminOrIsOwner(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return request.user and request.user.is_staff
+    def has_object_permission(self, request, view, obj=None):
+        print(f"User: {request.user}, Object owner: {obj.owner}")
+        if obj.status == "Application":
+            return request.user == obj.owner or request.user.is_staff
+        return True
